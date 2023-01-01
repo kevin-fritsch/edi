6ba7d8884f41b1f9fe91c5b2b24a179c538a70c8
Kevin <kevin@fritsch.vip>
2023-01-01 09:19:39 +0100

    LAZYFIXES

--- account_invoice_import/wizard/account_invoice_import.py
+++ account_invoice_import/wizard/account_invoice_import.py
@@ -1086,10 +1086,8 @@ class AccountInvoiceImport(models.TransientModel):
                         copy_dict["product_id"] = False
                     # Add the adjustment line
                     iline.with_context(check_move_validity=False).copy(copy_dict)
-                    invoice.with_context(
-                        check_move_validity=False
-                    )._recompute_dynamic_lines(recompute_all_taxes=True)
-                    invoice._check_balanced()
+                    container = {'records': self, 'self': self}
+                    invoice._check_balanced(container)
                     logger.info("Adjustment invoice line created")
         # Fallback: create global ajustment line
         if float_compare(
@@ -1115,10 +1113,8 @@ class AccountInvoiceImport(models.TransientModel):
                 .with_context(check_move_validity=False)
                 .create(il_vals)
             )
-            invoice.with_context(check_move_validity=False)._recompute_dynamic_lines(
-                recompute_all_taxes=True
-            )
-            invoice._check_balanced()
+            container = {'records': invoice, 'self': invoice}
+            invoice._check_balanced(container)
             logger.info("Global adjustment invoice line created ID %d", mline.id)
         assert not float_compare(
             parsed_inv["amount_untaxed"],
@@ -1177,10 +1173,8 @@ class AccountInvoiceImport(models.TransientModel):
                         vals["credit"] = new_balance * -1
                     logger.info("Force VAT amount with diff=%s", diff_tax_amount)
                     mline.with_context(check_move_validity=False).write(vals)
-                    invoice.with_context(
-                        check_move_validity=False
-                    )._recompute_dynamic_lines()
-                    invoice._check_balanced()
+                    container = {'records': invoice, 'self': invoice}
+                    invoice._check_balanced(container)
                     break
             if not has_tax_line:
                 raise UserError(
